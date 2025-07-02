e48bce54c642cd43b62bdcea8ca5303d9ef91433
Marcelo de Oliveira <p-marcelo.oliveira@a5x.com.br>
2025-07-02 09:11:04 -0300

    Implementação Carrossel Banner - EXTP-127 Home - Carrossel Banner Comunicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Topbar from "./components/Topbar";
 import Sidebar from "./components/Sidebar";
 import BreadcrumbsComponent from "./components/BreadCrumber/BreadCrumber";
+import CarrosselBanner from "./components/CarrosselBanner/CarrosselBanner";
 
 export default function App() {
   return (
@@ -9,6 +10,9 @@ export default function App() {
       <BreadcrumbsComponent />
       <div style={{ display: 'flex', flex: 1 }}>
         <Sidebar />
+        <div style={{ flex: 1, padding: '20px', overflow: 'auto' }}>
+          <CarrosselBanner />
+        </div>
       </div>
     </div>
   );
