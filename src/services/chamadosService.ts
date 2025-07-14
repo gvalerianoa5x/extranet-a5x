5bbea5b3b51d5e609558d13e0b29dddc403281a1
--global <p-guilherme.valeriano@a5x.com.br>
2025-07-14 16:11:46 -0300

    fix:Teste de cors

--- src/services/chamadosService.ts
+++ src/services/chamadosService.ts
@@ -1,6 +1,7 @@
-import dataApi from './apis/apiBase';
+import apiLowCode from './apis/apiLowcode';
 
 export const getMyTickets = async () => {
-  const { data } = await dataApi.get('v1/ticket/my-tickets');
+  //const { data } = await dataApi.get('v1/ticket/my-tickets');
+  const { data } = await apiLowCode.get('a5x/user');
   return data;
 };
