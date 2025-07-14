d59ba09b5a142b598bb94a6c33192352bd6b9c7b
--global <p-guilherme.valeriano@a5x.com.br>
2025-07-14 18:32:13 -0300

    fix: Global token setado para as rotas

--- src/services/apis/apiEsi.ts
+++ src/services/apis/apiEsi.ts
@@ -1,7 +1,17 @@
 import axios from 'axios';
+import { useAuth } from '../../contexts/AuthProvider';
 
 const apiEsi = axios.create({
   baseURL: 'https://a5x-dev.4biz.one/lowcode/integrations/',
+});
+
+apiEsi.interceptors.request.use(config => {
+  const { token } = useAuth();
+  const tokenAuth = token || localStorage.getItem('HYPER-AUTH-TOKEN'); // ou outro lugar que você armazena o token
+  if (token) {
+    config.headers.Authorization = `Bearer ${tokenAuth}`;
+  }
+  return config;
 });
 
 export default apiEsi;
